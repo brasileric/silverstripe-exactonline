594d956d41d08b7c0a24662451ec21f7fb7ab69e
Eric Hes <eric@hestec.nl>
2018-04-16 18:26:07 +0200

    init

---
+++ src/ExactController.php
@@ -0,0 +1,50 @@
+<?php
+
+use SilverStripe\Control\Controller;
+use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Security\Member;
+
+class ExactController extends Controller {
+
+    private static $allowed_actions = array (
+        'Authorize',
+        'Setup'
+    );
+
+    public function Authorize() {
+
+        $code = $_GET['code'];
+
+        if (isset($code) && Member::currentUserID()){
+        //if (1 == 2){
+            $config = SiteConfig::get()->byID(1);
+            $config->ExactOauth = $code;
+            //$config->ExactOauth = "1234";
+            $config->write();
+
+            return "goed";
+            //return $_GET['code'];
+        }
+
+        //return SiteConfig::current_site_config()->GlobalFromEmail;
+        //return SiteConfig::current_site_config()->GlobalFromEmail();
+
+        return "fout";
+
+    }
+
+    public function Setup() {
+
+        $connection = new \Picqer\Financials\Exact\Connection();
+        $connection->setRedirectUrl('http://www.ss-boilerplate.hst1.nl/ExactController/Authorize'); // Same as entered online in the App Center
+        $connection->setExactClientId('a9804607-e1b6-4a7b-9ec8-8c670e822510');
+        $connection->setExactClientSecret('DSG5SPofzWny');
+        $connection->redirectForAuthorization();
+
+        //return SiteConfig::current_site_config()->GlobalFromEmail;
+        //return SiteConfig::current_site_config()->GlobalFromEmail();
+
+    }
+
+
+}
