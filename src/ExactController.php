52fd8d4f4b3b740def04fd3a38d72898e257351a
Eric Hes <eric@hestec.nl>
2018-04-23 19:02:28 +0200

    updates

--- src/ExactController.php
+++ src/ExactController.php
@@ -10,6 +10,8 @@ class ExactController extends Controller {
 
     private static $allowed_actions = array (
         'Authorize',
+        'Authorize2',
+        'Authorize3',
         'Connect',
         'Disconnect'
     );
@@ -26,19 +28,64 @@ class ExactController extends Controller {
 
     public function Authorize() {
 
-        if (strlen($_GET['code']) > 100 && is_numeric($this->getRequest()->param('ID')) && Member::currentUserID() && $this->isCmsMember()){
+        $connconfig = ExactOnlineConnection::config();
+
+        /*if (strlen($_GET['code']) > 100 && is_numeric($this->getRequest()->param('ID')) && Member::currentUserID() && $this->isCmsMember()){
 
             $connconfig = ExactOnlineConnection::get()->byID($this->getRequest()->param('ID'));
             $connconfig->OauthCode = $_GET['code'];
             $connconfig->write();
 
-            $this->redirect(Director::absoluteBaseURL()."admin/exactonline/Hestec-ExactOnline-ExactOnlineConnection");
+            //$this->redirect(Director::absoluteBaseURL() . "ExactController/Authorize2/".$this->getRequest()->param('ID'));
         }
 
         return '<p>There was an error, <a href="'.Director::absoluteBaseURL().'admin/exactonline/Hestec-ExactOnline-ExactOnlineConnection">CLICK HERE</a> to go back and try again</p>';
         //$this->getRequest()->getSession()->set('ConnObjectID', 2);
 
+        //return $this->getRequest()->getSession()->get('ConnObjectID');*/
+
+    }
+
+    public function Authorize2() {
+
+        $connconfig = ExactOnlineConnection::config();
+
+        $connobject = ExactOnlineConnection::get()->byID($this->getRequest()->param('ID'));
+
+        //if (strlen($_GET['code']) > 100 && is_numeric($this->getRequest()->param('ID')) && Member::currentUserID() && $this->isCmsMember()){
+
+            $connection = new \Picqer\Financials\Exact\Connection();
+            $connection->setRedirectUrl(Director::absoluteBaseURL() . "ExactController/Authorize/".$this->getRequest()->param('ID'));
+            $connection->setExactClientId($connconfig->ClientId);
+            $connection->setExactClientSecret($connconfig->ClientSecret);
+
+            $connection->setAuthorizationCode("5F33%21IAAAABQ1EUQwJ72GLjmoOw_Aaidrh6IbFkIe9mZbFoJ4ZAyG8QAAAAHeIwwubzC0WMc__bzHEI5jrjRp5942k03CZJuaeX9M2fKFvQP51b24UQ4ha5-zqgSAZMBbCL8JBsUbn7bQ3C-yrQdbtfxEBMmdMFpzPQX_ifuZs8vD-Qo3wrXaB0R41VlUhhW6idgWS4EiXvHIJN70dNBqf1eFzI4-3zB6rrFxJDBJNBFuguL8umMYhVYBBs57n_mm-mjT2dh48sdSs9h5EQwmDv1PUfcrEDl0twzEY7MsJizKDCoRdszZH4lfgUed6TSlshhT3O7XFZQ4Amu3pJMR7iFBdDCGqQekvivCBcFoMB_fR2M7vMMVXuJpTJI");
+            //$connection->setAuthorizationCode("");
+
+            try {
+                $connection->connect();
+            } catch (\Exception $e)
+            {
+                throw new Exception('Could not connect to Exact: ' . $e->getMessage());
+            }
+
+            //return $connconfig->OauthCode;
+            //$connconfig->AccessToken = serialize($connection->getAccessToken());
+            //$connconfig->RefreshToken = $connection->getRefreshToken();
+            //$connconfig->write();
+
+            //$this->redirect(Director::absoluteBaseURL()."admin/exactonline/Hestec-ExactOnline-ExactOnlineConnection");
+        //}
+
+        return '<p>There was an error, <a href="'.Director::absoluteBaseURL().'admin/exactonline/Hestec-ExactOnline-ExactOnlineConnection">CLICK HERE</a> to go back and try again</p>';
+
         //return $this->getRequest()->getSession()->get('ConnObjectID');
+
+    }
+
+    public function Authorize3() {
+
+        return "TEST";
 
     }
 
